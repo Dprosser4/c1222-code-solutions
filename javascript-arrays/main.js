57e0b21b9e554199c11c84f83fe5488d8624168b
Dylan Prosser <dprosser4@gmail.com>
2022-12-12 15:57:26 -0800

    completed javascript-arrays

---
+++ javascript-arrays/main.js
@@ -0,0 +1,28 @@
+var colors = ['red', 'white', 'blue']
+
+console.log('value of [0]', colors[0]);
+console.log('value of [1]', colors[1]);
+console.log('value of [2]', colors[2]);
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+
+colors[2] = 'green';
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
+
+console.log('value of colors:', colors);
+
+
+var students = ['Dylan', 'Joseph', 'Jorden', 'Alex'];
+
+var numberOfStudents = students.length;
+
+console.log('value of numberOfStudents', numberOfStudents);
+
+lastIndex = numberOfStudents - 1;
+
+lastStudent = students[lastIndex];
+
+console.log('The last student in the array is: ' + lastStudent);
+
+console.log('value of students:', students );
