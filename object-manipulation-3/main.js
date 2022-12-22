453c984b543cfa0f293412c825c3e9066fe25493
Dylan Prosser <dprosser4@gmail.com>
2022-12-22 11:44:22 -0800

    object-manipulation-3 in progress

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -3,10 +3,25 @@ console.log('Lodash is loaded:', typeof _ !== 'undefined');
 test cases:
 playTheGame(['Dylan', 'Cody', 'Josh', 'Joseph'] , 2)
 playTheGame(['Cody', 'Alex', 'Jimmy', 'Jordan'], 3)
+playTheGame(['Dylan', 'Cody', 'Josh', 'Joseph'] , 12)
+playTheGame(['Dylan', 'Cody', 'Josh', 'Joseph'] , 13)
 playTheGame(['Cody', 'Alex', 'Jimmy', 'Jordan', 'Matt'], 3)
 playTheGame(['Dylan', 'Cody',] , 2)
 
 */
+
+/*
+recieves array of strings that will be the player names
+  guard agianst more than 4 players
+  create an empty array to store player objects
+  looks at each string
+  create a temporary object to hold new player
+  create a name property of the current iteration player string
+  create a hand property with no value
+  push the whole object to the players array
+  return players
+*/
+
 function createPlayerObjects(array){
   if (array.length > 4){
     console.log('Input must contain no more than 4 players!')
@@ -21,6 +36,26 @@ function createPlayerObjects(array){
   }
   return players
 }
+
+/*
+this function creates a deck of cards as objects stored in an array and shuffles it
+  create an array containing all the possible card ranks
+  create an array containing all the possible card suits
+  create an empty array to store all the card ojects
+  create an empty array to store the deck once its shuffled
+  look at each item in the card ranks array in order
+    look at ech item in the suits array in order
+    create a temporary object to store the new card
+      add a rank property to the temporay object with the card value of the current iteration of the ranks loop
+      add a rank property to the temporay object with the card value of the current iteration of the suits loop
+      add a points property to each card based on how long the string name is
+      if less the string length is 3 points should be 11 (ace)
+      if the the string length is longer than 3 points should be 10 (face cards)
+      if the string length is shorter than 3 points equals the value of the string as a number
+      push the temporary object to the deck array
+   shuffle the deck with the lodash shuffle method (Fisher–Yates shuffle)
+   return the deck
+*/
 
 function createDeckObject(){
   var cardRanks = ['2','3','4','5','6','7','8','9','10', 'Jack', 'Queen', 'King', 'Ace']
@@ -47,6 +82,10 @@ function createDeckObject(){
   return deck
 }
 
+/*
+
+*/
+
 
 function playTheGame(array, numberofcards) {
   if (numberofcards > 13) {
@@ -56,13 +95,17 @@ function playTheGame(array, numberofcards) {
   var cardsDealt = numberofcards
   var gameDeck = createDeckObject()
   var gamePlayers = createPlayerObjects(array)
+
+
   for (var z = 1; z < array.length + 1; z++){
     var tempArr = []
     for (var q = 1; q < numberofcards + 1; q++){
-      tempArr.push(gameDeck[z * q])
+      tempArr.push(gameDeck.shift())
     }
     gamePlayers[z - 1]['hand'] = tempArr
   }
+
+
   console.log(gamePlayers)
   for (var x = 0; x < gamePlayers.length; x++){
     var score = 0
