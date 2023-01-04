0d50616b3396882766e6cd7a6715571f36df90a0
Dylan Prosser <dprosser4@gmail.com>
2023-01-04 18:39:27 +0000

    completed javascript-timers

---
+++ javascript-timers/interval.js
@@ -0,0 +1,15 @@
+
+setInterval(changeDisplay, 1000);
+
+var $countdownDisplay = document.querySelector('.countdown-display');
+
+function changeDisplay() {
+  if ($countdownDisplay.textContent > 1) {
+    var displayNum = $countdownDisplay.textContent;
+    displayNum--;
+    $countdownDisplay.textContent = displayNum;
+  } else {
+    $countdownDisplay.textContent = '~Earth Beeeelooowww Us~';
+  }
+
+}
---
+++ javascript-timers/timeout.js
@@ -0,0 +1,8 @@
+
+setTimeout(changeMessage, 2000);
+
+var $message = document.querySelector('.message');
+
+function changeMessage() {
+  $message.textContent = 'Hello World';
+}
